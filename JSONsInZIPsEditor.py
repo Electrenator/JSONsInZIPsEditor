146871a950936c252fdfda31586098ce74144a0d
Electrenator <electrenator2000@protonmail.com>
2019-10-31 22:42:18 +0100

    Uploading first working version
    Is actually the second working version because needed to add utf-8 support for some of my test files but who really cares :P

---
+++ JSONsInZIPsEditor.py
@@ -0,0 +1,153 @@
+from zipfile import ZipFile as zipFile 
+import os
+import random
+import json
+import time
+import shutil
+
+''' TODO:
+        - Add function to skip broken json files and display it at end
+        - Add function to skip broken zip files and display it at end
+'''
+
+def searchDirFor(directory, startsW, endsW):
+    ''' Searches recursively in the specified directory for files that start with "startW" and end with "endsW" '''
+    foundTarget = []
+
+    for file in os.listdir(directory):
+        if (file.endswith(endsW) and file.startswith(startsW)):
+            foundTarget.append(os.path.join(directory, file));
+
+        elif (os.path.isdir(os.path.join(directory, file))):
+            foundTarget += searchDirFor(os.path.join(directory, file), startsW, endsW)
+    return foundTarget
+
+
+def displayArray(array, description):
+    ''' Does as the name says with a nice & readable format '''
+    temp = ''
+    for i in range(len(array)):
+        if len(temp) == 0:
+            temp += '"'+array[i]+'"'
+        elif i+1 == len(array):
+            temp += ' and "'+array[i]+'"'
+        else:
+            temp += ', "'+array[i]+'"'
+    print(timeStamp(timeStart),description+':', temp)
+
+
+def getFileName(directory):
+    ''' Gets the name of the specified file without extension and dictionary '''
+    splitUpDirectory = allZips[i].split('/')
+    fileName = splitUpDirectory[len(splitUpDirectory)-1].rsplit('.', 1)[0]
+    return fileName
+
+
+def jsonChangeValue(file, key, value):
+    ''' Reads specified json file, changes it and saves that change '''
+    print(f'{timeStamp(timeStart)} Changing \'{key}\' to \'{value}\' in "{file}"', end='...    ')
+    try:
+        with open(file, 'r+') as f:
+            try:
+                data = json.loads(f.read())
+                f.seek(0)
+                data = searchAndReplace(data, key, value)
+
+                json.dump(data, f, indent=4)
+            finally:
+                f.close()
+    except ValueError:
+        with open(file, 'r+', encoding='utf-8-sig') as f:
+            try:
+                data = json.loads(f.read())
+                f.seek(0)
+                data = searchAndReplace(data, key, value)
+
+                json.dump(data, f, indent=4)
+            finally:
+                f.close()
+    print('done!')
+
+
+def searchAndReplace(var, searchKey, newValue):
+    ''' Searches and replaces values in a dictionary with the specified key '''
+    if type(var) == dict:
+        for key in var.keys():
+            if key == searchKey:
+                var[key] = newValue
+            elif type(var[key]) == dict:
+                var[key] = searchAndReplace(var[key], searchKey, newValue)
+        return var            
+    else:
+        raise TypeError('Input not a dict (the only dicts are suported)')
+
+
+def timeStamp(timeStart):
+    return f'[{round(time.time()-timeStart, 3)}s]'
+
+
+def printEnd():
+    return "...    "
+
+
+timeStart = time.time()
+allZips = searchDirFor('./', '', '.zip')
+displayArray(allZips, f'Found {len(allZips)} zips')
+
+
+# Gets temporary directory
+tempDir = './temp'
+while os.path.isdir(tempDir) == 1:
+    print(f'{timeStamp(timeStart)} Diractory "{tempDir}/" already exists, looking for new temp directory')
+    tempDir += str(random.randint(0,9))
+
+print(f'{timeStamp(timeStart)} Using temporary directory: "{tempDir}/"')
+
+
+# Processes all the zip files
+for i in range(len(allZips)):
+    # Get temporary current directory
+    tempCurrentDir = os.path.join(tempDir, getFileName(allZips[i]))
+
+    # Extract currently processing zip file
+    print(f'\n{timeStamp(timeStart)} Extracting zip {i+1}: "{allZips[i]}" -> "{tempCurrentDir}/"',
+            end='...    ')
+    with zipFile(allZips[i], 'r') as zip:
+        try:
+            zip.extractall(tempCurrentDir)
+        finally:
+            zip.close()
+    print('done!')
+
+    # Get JSON files of extracted zip
+    allJsons = searchDirFor(tempCurrentDir, '', '.json')
+    displayArray(allJsons, f'Found {len(allJsons)} jsons')
+
+    # Look threw all JSONs and replace specefied thing
+    for j in range(len(allJsons)):
+        jsonChangeValue(allJsons[j], 'version', 404)
+
+    # Rezip extracted zip
+    print(f'{timeStamp(timeStart)} Writing: "{tempCurrentDir}/*" -> "{allZips[i-1]}"', end="...    ")
+    with zipFile(allZips[i], 'w') as zip:
+        try:
+            for j in os.listdir(tempCurrentDir):
+                zip.write(os.path.join(tempCurrentDir, j), arcname=j)
+        finally:
+            zip.close()
+    print('done!')
+
+    # Remove temp dir for zip
+    print(f'{timeStamp(timeStart)} Removing: "{tempCurrentDir}"', end=printEnd())
+    shutil.rmtree(tempCurrentDir)
+    print('done!')
+
+
+# Remove temp dir fully
+print(f'\n{timeStamp(timeStart)} Removing the temp directory: "{tempDir}"', end=printEnd())
+os.rmdir(tempDir)
+print('done!')
+
+
+print('\n'*2+'-='*5, 'SCRIPT FINISHED', '=-'*5)
+print(f'\nExecution took: {round(time.time()-timeStart, 6)} seconds')
