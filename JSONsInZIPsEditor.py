c946de850864069e6928297c2a7ff47628a5b377
Electrenator <electrenator2000@protonmail.com>
2019-11-13 21:27:20 +0100

    Added support for function importation from JSONsInZIPsEditor
    Also fixed another mistake in the README, this time about the JSON format, and added a .gitignore so that the __pycache__ folder that's created when importing won't get uploaded

--- JSONsInZIPsEditor.py
+++ JSONsInZIPsEditor.py
@@ -3,7 +3,6 @@ from zipfile import ZipFile as zipFile
 import os, random, json, time, shutil
 
 
-
 def searchDirFor(directory, startsW, endsW):
     ''' Searches recursively in the specified directory for files that start with "startW" and end with "endsW" '''
     foundTarget = []
@@ -32,12 +31,24 @@ def displayArray(array, description):
 
 def getFileName(directory):
     ''' Gets the name of the specified file without extension and dictionary '''
-    splitUpDirectory = allZips[i].split('/')
+    splitUpDirectory = directory.split('/')
     fileName = splitUpDirectory[len(splitUpDirectory)-1].rsplit('.', 1)[0]
     return fileName
 
 
 def jsonChangeValue(file, key, value):
+
+
+
+    '''TODO:
+            - Rewrite this so that things with less lines can overwrite things with more lines
+                \ like: list to string or into
+                    \ Will now give invalid JSON becouse it doesn't take into account the length of a file before filling.
+                        \ may be fixed by reading than emptying and then writing in that order instead of all at ones in the function.
+    '''
+
+
+
     ''' Reads specified json file, changes it and saves that change '''
     print(f'{timeStamp(timeStart)} Changing \'{key}\' to \'{value}\' in "{file}"', end=printEnd())
     try:
@@ -115,105 +126,115 @@ def getConformation(q):
         return getConformation(q)
 
 
-# Get user input
-try:
-    inputArray = getInputJSON()
-    inputKey = inputArray[0]
-    inputData = json.loads(inputArray[1])
-    inputValue = inputData.get(inputKey)
-except KeyboardInterrupt:
-    print("")
-    exit(0)
+def main():
+    # Set variables that are global (mostly for writing not neded for reading)
+    global timeStart
+
+    # Get user input
+    try:
+        inputArray = getInputJSON()
+        inputKey = inputArray[0]
+        inputData = json.loads(inputArray[1])
+        inputValue = inputData.get(inputKey)
+    except KeyboardInterrupt:
+        print("")
+        exit(0)
 
 
-# Imput conformation
-conf = getConformation(f"\nThe key {inputKey} will be set to \"{inputValue}\". This will result in:\n{inputData}\n\nAre you sure this is what you want?")
+    # Imput conformation
+    conf = getConformation(f"\nThe key {inputKey} will be set to \"{inputValue}\". This will result in:\n{inputData}\n\nAre you sure this is what you want?")
 
-if conf == False:
-    print("Exiting...")
-    exit(0)
-print("\n")
+    if conf == False:
+        print("Exiting...")
+        exit(0)
+    print("\n")
 
-# Start timer
-timeStart = time.time()
-
-
-# Get zips in current and lower dirs
-allZips = searchDirFor('./', '', '.zip')
-displayArray(allZips, f'Found {len(allZips)} zips')
+    # Start timer
+    timeStart = time.time()
 
 
-# Gets temporary directory
-tempDir = './temp'
-while os.path.isdir(tempDir) == 1:
-    print(f'{timeStamp(timeStart)} Diractory "{tempDir}/" already exists, looking for new temp directory')
-    tempDir += str(random.randint(0,9))
-
-print(f'{timeStamp(timeStart)} Using temporary directory: "{tempDir}/"')
+    # Get zips in current and lower dirs
+    allZips = searchDirFor('./', '', '.zip')
+    displayArray(allZips, f'Found {len(allZips)} zips')
 
 
-# Processes all the zip files
-failed = [["Operation", "File", "Error type", "Error value"]]
-for i in range(len(allZips)):
-    # Get temporary current directory
-    tempCurrentDir = os.path.join(tempDir, getFileName(allZips[i]))
+    # Gets temporary directory
+    tempDir = './temp'
+    while os.path.isdir(tempDir) == 1:
+        print(f'{timeStamp(timeStart)} Diractory "{tempDir}/" already exists, looking for new temp directory')
+        tempDir += str(random.randint(0,9))
 
-    # Extract currently processing zip file
-    print(f'\n{timeStamp(timeStart)} Extracting zip {i+1}: "{allZips[i]}" -> "{tempCurrentDir}/"', end=printEnd())
-    with zipFile(allZips[i], 'r') as zip:
-        try:
-            zip.extractall(tempCurrentDir)
-        except Exception as e:
-            failed.append(["extracting", allZips[i], type(e).__name__, str(e)])
-        finally:
-            zip.close()
-    print('done!')
-
-    # Get JSON files of extracted zip
-    allJsons = searchDirFor(tempCurrentDir, '', '.json')
-    displayArray(allJsons, f'Found {len(allJsons)} jsons')
-
-    # Look threw all JSONs and replace specefied thing
-    for j in range(len(allJsons)):
-        try:
-            jsonChangeValue(allJsons[j], inputKey, inputValue)
-        except Exception as e:
-            print("failed!")
-            failed.append(["changing json", allJsons[j], type(e).__name__, str(e)])
-
-    # Rezip extracted zip
-    print(f'{timeStamp(timeStart)} Writing: "{tempCurrentDir}/*" -> "{allZips[i-1]}"', end=printEnd())
-    with zipFile(allZips[i], 'w') as zip:
-        try:
-            for j in os.listdir(tempCurrentDir):
-                zip.write(os.path.join(tempCurrentDir, j), arcname=j)
-        except Excepton as e:
-            failed.append(["zipping", os.path.join(tempCurrentDir, j), type(e).__name__, str(e)])
-        finally:
-            zip.close()
-    print('done!')
-
-    # Remove temp dir for zip
-    print(f'{timeStamp(timeStart)} Removing: "{tempCurrentDir}"', end=printEnd())
-    shutil.rmtree(tempCurrentDir)
-    print('done!')
+    print(f'{timeStamp(timeStart)} Using temporary directory: "{tempDir}/"')
 
 
-# Remove temp dir fully
-print(f'\n{timeStamp(timeStart)} Removing the temp directory: "{tempDir}"', end=printEnd())
-try:
-    os.rmdir(tempDir)
-    print('done!')
-except Exception as e:
-    print('failed!')
-    failed.append(["removing", tempDir, type(e).__name__, str(e)])
+    # Processes all the zip files
+    failed = [["Operation", "File", "Error type", "Error value"]]
+    for i in range(len(allZips)):
+        # Get temporary current directory
+        tempCurrentDir = os.path.join(tempDir, getFileName(allZips[i]))
 
-print('\n'*2+'-='*5, 'SCRIPT FINISHED', '=-'*5+'\n')
+        # Extract currently processing zip file
+        print(f'\n{timeStamp(timeStart)} Extracting zip {i+1}: "{allZips[i]}" -> "{tempCurrentDir}/"', end=printEnd())
+        with zipFile(allZips[i], 'r') as zip:
+            try:
+                zip.extractall(tempCurrentDir)
+            except Exception as e:
+                failed.append(["extracting", allZips[i], type(e).__name__, str(e)])
+            finally:
+                zip.close()
+        print('done!')
 
-if len(failed) > 1:
-    temp = 'S' if len(failed)==1 else ''
-    print(f'WITH ERROR{temp}'+'=-'*4)
-    for i in range(len(failed)-1):
-        print(f'\tGot {failed[i+1][2]} error "{failed[i+1][3]}" while {failed[i+1][0]} file "{failed[i+1][1]}"\n')
+        # Get JSON files of extracted zip
+        allJsons = searchDirFor(tempCurrentDir, '', '.json')
+        displayArray(allJsons, f'Found {len(allJsons)} jsons')
 
-print(f'\nExecution of script took: {round(time.time()-timeStart, 6)} seconds\n')
+        # Look threw all JSONs and replace specefied thing
+        for j in range(len(allJsons)):
+            try:
+                jsonChangeValue(allJsons[j], inputKey, inputValue)
+            except Exception as e:
+                print("failed!")
+                failed.append(["changing json", allJsons[j], type(e).__name__, str(e)])
+
+        # Rezip extracted zip
+        print(f'{timeStamp(timeStart)} Writing: "{tempCurrentDir}/*" -> "{allZips[i-1]}"', end=printEnd())
+        with zipFile(allZips[i], 'w') as zip:
+            try:
+                for j in os.listdir(tempCurrentDir):
+                    zip.write(os.path.join(tempCurrentDir, j), arcname=j)
+            except Excepton as e:
+                failed.append(["zipping", os.path.join(tempCurrentDir, j), type(e).__name__, str(e)])
+            finally:
+                zip.close()
+        print('done!')
+
+        # Remove temp dir for zip
+        print(f'{timeStamp(timeStart)} Removing: "{tempCurrentDir}"', end=printEnd())
+        shutil.rmtree(tempCurrentDir)
+        print('done!')
+
+
+    # Remove temp dir fully
+    print(f'\n{timeStamp(timeStart)} Removing the temp directory: "{tempDir}"', end=printEnd())
+    try:
+        os.rmdir(tempDir)
+        print('done!')
+    except Exception as e:
+        print('failed!')
+        failed.append(["removing", tempDir, type(e).__name__, str(e)])
+
+    print('\n'*2+'-='*5, 'SCRIPT FINISHED', '=-'*5+'\n')
+
+    if len(failed) > 1:
+        temp = 'S' if len(failed)==1 else ''
+        print(f'WITH ERROR{temp}'+'=-'*4)
+        for i in range(len(failed)-1):
+            print(f'\tGot {failed[i+1][2]} error "{failed[i+1][3]}" while {failed[i+1][0]} file "{failed[i+1][1]}"\n')
+
+    print(f'\nExecution of script took: {round(time.time()-timeStart, 6)} seconds\n')
+
+
+# Run main() if file wasn't imported
+if __name__ == '__main__':
+    timeStart = None # Will be set in main() but is a global definition for other functions
+    main()
